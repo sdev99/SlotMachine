ce6f77155669d8a5bbfafab1a142a29e35a61fe7
Sukhdev Patidar <sukhdevpatidar@Sukhdevs-MacBook-Air.local>
2020-02-16 00:00:05 +0530

    added user form

--- src/pages-admin/Dashboard/Dashboard.js
+++ src/pages-admin/Dashboard/Dashboard.js
@@ -22,7 +22,15 @@ import {
     InputGroupAddon,
     InputGroupText,
     FormInput,
-    Button
+    Button,
+    Modal,
+    ModalBody,
+    ModalHeader,
+    ModalFooter,
+    Container,
+    Row,
+    Col,
+    FormCheckbox
 } from "shards-react";
 
 class Dashboard extends Component {
@@ -31,7 +39,18 @@ class Dashboard extends Component {
         userPassword: "",
         rememberMe: false,
         loading: false,
-        usersData: [1, 2, 3, 4, 5, 6, 7]
+        showUserDataModel: false,
+        usersData: [1, 2, 3, 4, 5, 6, 7],
+
+        affiliatedWithNapaStore: false,
+        anInstallerCustomer: false,
+
+        napaNewElectrical: false,
+        wilson: false,
+        premiumPlus: false,
+        premiumSteering: false,
+        powerSupport: false,
+        newSteering: false,
     };
 
 
@@ -95,7 +114,9 @@ class Dashboard extends Component {
                                         <td>Test Store</td>
                                         <td>Test Address</td>
                                         <td className={'options'}>
-                                            <Button className={'option-btn'}><FontAwesomeIcon icon={faEdit}/></Button>
+                                            <Button className={'option-btn'} onClick={() => {
+                                                this.setState({showUserDataModel: true});
+                                            }}><FontAwesomeIcon icon={faEdit}/></Button>
                                             <Button className={'option-btn delete'}><FontAwesomeIcon
                                                 icon={faTrash}/></Button>
                                             <Button className={'option-btn detail'}><FontAwesomeIcon
@@ -108,6 +129,199 @@ class Dashboard extends Component {
                         </tbody>
                     </table>
                 </div>
+
+
+                <Modal centered={true} className={'userdata-edit-modal'} open={this.state.showUserDataModel} toggle={() => {
+                    this.setState({showUserDataModel: !this.state.showUserDataModel});
+                }}>
+                    <ModalHeader>Edit User Data</ModalHeader>
+                    <ModalBody>
+                        <div className={'form-container'}>
+                            <Container className={'option-form'}>
+                                <Row>
+                                    <Col>
+                                        <div className={'heading4'}>
+                                            ARE YOU?
+                                        </div>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.affiliatedWithNapaStore}
+                                            onChange={e => {
+                                                this.setState({
+                                                    affiliatedWithNapaStore: true,
+                                                    anInstallerCustomer: false
+                                                });
+                                            }}
+                                        >affiliated with a NAPA store</FormCheckbox>
+                                    </Col>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.anInstallerCustomer}
+                                            onChange={e => {
+                                                this.setState({
+                                                    affiliatedWithNapaStore: false,
+                                                    anInstallerCustomer: true
+                                                });
+                                            }}
+                                        >
+                                            an installer customer
+                                        </FormCheckbox>
+                                    </Col>
+                                </Row>
+                            </Container>
+
+                            <Container className={'input-form'}>
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Full Name</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Email address</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Store number</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Servicing DC</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Store Name</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={4}>
+                                        <span className={'input-label'}>Store address</span>
+                                    </Col>
+                                    <Col sm={12} md={8}>
+                                        <FormInput className="input-field"/>
+                                    </Col>
+                                </Row>
+                            </Container>
+
+
+                            <Container className={'product-choose-form'}>
+                                <Row>
+                                    <Col>
+                                        <div className={'heading4'}>
+                                            PRODUCTS ARE YOU CURRENTLY BUYING FROM NAPA
+                                        </div>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.napaNewElectrical}
+                                            onChange={e => {
+                                                this.setState({napaNewElectrical: !this.state.napaNewElectrical});
+                                            }}
+                                        >
+                                            NAPA NEW Electrical
+                                        </FormCheckbox>
+                                    </Col>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.wilson}
+                                            onChange={e => {
+                                                this.setState({wilson: !this.state.wilson});
+                                            }}
+                                        >
+                                            Wilson
+                                        </FormCheckbox>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.premiumPlus}
+                                            onChange={e => {
+                                                this.setState({premiumPlus: !this.state.premiumPlus});
+                                            }}
+                                        >
+                                            NAPA Power Premium Plus
+                                        </FormCheckbox>
+                                    </Col>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.premiumSteering}
+                                            onChange={e => {
+                                                this.setState({premiumSteering: !this.state.premiumSteering});
+                                            }}
+                                        >
+                                            Premium NAPA Steering
+                                        </FormCheckbox>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.powerSupport}
+                                            onChange={e => {
+                                                this.setState({powerSupport: !this.state.powerSupport});
+                                            }}
+                                        >
+                                            NAPA Power Sport
+                                        </FormCheckbox>
+                                    </Col>
+                                    <Col sm={12} md={6}>
+                                        <FormCheckbox
+                                            checked={this.state.newSteering}
+                                            onChange={e => {
+                                                this.setState({newSteering: !this.state.newSteering});
+                                            }}
+                                        >
+                                            NAPA NEW Steering
+                                        </FormCheckbox>
+                                    </Col>
+                                </Row>
+
+                            </Container>
+
+
+
+                        </div>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button block className={'play-btn'}  onClick={() => {
+                            this.setState({showUserDataModel: false});
+                        }}>Save Detail</Button>
+                    </ModalFooter>
+                </Modal>
             </LoadingOverlay>
 
         );
