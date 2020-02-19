3bf63d86f6bd8ef33f220c5e75910a822a24f528
Sukhdev Patidar <sukhdevpatidar@Sukhdevs-MacBook-Air.local>
2020-02-19 21:29:11 +0530

    merge

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const MODELS = require('./api/models/apiModel')
 const path = require('path');
-const apiPort = process.env.PORT || 5000;
+const apiPort = 5000;
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -13,32 +13,20 @@ mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb+srv://sukhdevpatidar:sukhdevpatidar@cluster0-4ojpw.mongodb.net/skillstoreapi?retryWrites=true&w=majority");
 mongoose.connect("mongodb://heroku_w72khd5w:heroku_w72khd5w@ds123181.mlab.com:23181/heroku_w72khd5w");
 
-app.use(cors({origin: '*'}));
-app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-// app.use(express.static('./'));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    console.log("PRODUCTION");
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
-} else {
-    console.log("DEVELOPMENT");
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/public/index.html'));
-    })
-}
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({origin: '*'}));
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static('./'));
 
 var routes = require('./api/routes/apiRoutes'); //importing route
 routes(app); //register the route
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
