a1a1efe7f0751732ad65da0a1882997da10463e2
Sukhdev Patidar <sukhdevpatidar@Sukhdevs-MacBook-Air.local>
2020-02-21 01:21:57 +0530

    changed api url to live

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,4 @@
 export * from './user.constants';
 export * from './products.constants';
-// export const apiUrl = 'https://napa2020.herokuapp.com/api/';
-export const apiUrl = 'http://localhost:5000/api/';
+export const apiUrl = 'https://napa2020.herokuapp.com/api/';
+// export const apiUrl = 'http://localhost:5000/api/';
